5cb63afdaa306f17b061dc30b77d51236b24cb83
jackie174 <pcheng1@ualberta.ca>
2022-05-09 23:58:24 -0600

    update

---
+++ js/navbar.js
@@ -0,0 +1,25 @@
+let navbar = document.getElementById("main-nav");
+let navPos = navbar.getBoundingClientRect().top;
+let navbarLinks = document.querySelectorAll("nav a");
+console.log("--", navPos)
+window.addEventListener("scroll", e => {
+  
+    let scrollPos = window.scrollY;
+    if (scrollPos+100 > navPos) {
+        navbar.classList.add('sticky');
+        home.classList.add('navbarOffsetMargin');
+    } else {
+        navbar.classList.remove('sticky');
+        navbar.classList.remove('fixed');
+        home.classList.remove('navbarOffsetMargin');
+    }
+  
+    navbarLinks.forEach(link => {
+        let section = document.querySelector(link.hash);
+        if (scrollPos + 100 > section.offsetTop && scrollPos + 100 < section.offsetTop + section.offsetHeight ) {
+        link.classList.add("active");
+        } else {
+        link.classList.remove("active");
+        }
+    });
+});
