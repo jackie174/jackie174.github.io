12571e40bd92409c91ab5c3704b4233bdbe932bd
jackie174 <57372321+jackie174@users.noreply.github.com>
2022-12-25 01:32:31 -0700

    prevent animation after refresh

--- js/navbar.js
+++ js/navbar.js
@@ -23,3 +23,18 @@ window.addEventListener("scroll", e => {
         }
     });
 });
+
+// Get the element to animate
+const element = document.querySelector('.main-nav');
+// Check if the element has already been animated
+if (sessionStorage.getItem('animated') !== 'true') {
+    // If not, add the "animated" class to the element
+    element.classList.add('animated');
+
+    // Set the "animated" flag in session storage
+    sessionStorage.setItem('animated', 'true');
+  } else {
+    // If the element has already been animated, remove the "main-nav" class
+    element.style.opacity = 1;
+    document.querySelector(".main-nav").style.animation = 'none';
+  }
