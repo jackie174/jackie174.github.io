958139e9d3dd2d22939223f83efe1915f8df7c72
jackie174 <57372321+jackie174@users.noreply.github.com>
2022-12-24 23:48:16 -0700

    update navbar

--- js/navbar.js
+++ js/navbar.js
@@ -3,19 +3,20 @@ let navPos = navbar.getBoundingClientRect().top;
 let links = document.querySelectorAll("nav a");
 var links_array = [].slice.call(links);
 let navbarLinks = links_array.splice(0, 5);
+
+const home_image = document.getElementById('home');
+const navPosTop = home_image.offsetHeight;
+
 window.addEventListener("scroll", e => {
-  
     let scrollPos = window.scrollY;
-    if (scrollPos> navPos) {
+    if (scrollPos> navPosTop) {
         navbar.classList.add('sticky');
-        home.classList.add('navbarOffsetMargin');
     } else {
         navbar.classList.remove('sticky');
-        home.classList.remove('navbarOffsetMargin');
     }
     navbarLinks.forEach(link => {
         let section = document.querySelector(link.hash);
-        if (scrollPos > section.offsetTop && scrollPos < section.offsetTop + section.offsetHeight ) {
+        if (scrollPos+90 > section.offsetTop && scrollPos+90 < section.offsetTop + section.offsetHeight) {
         link.classList.add("active");
         } else {
         link.classList.remove("active");
